95e2ef2b11e6890423e533759ab27e8b5acf579c
Luiz Santos <luizcsbh@gmail.com>
2019-11-03 22:13:04 -0300

    Atualização
    Atualização do projeto

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -1,19 +1,24 @@
 function NoticiasDAO(connection){
-    this._connection = connection;
+	this._connection = connection;
 }
 
 NoticiasDAO.prototype.getNoticias = function(callback){
-    this._connection.query('SELECT * FROM noticias', callback);
+	this._connection.query('select * from noticias order by data_criacao desc', callback);
 }
 
-NoticiasDAO.prototype.getNoticia = function(callback){
-    this._connection.query('SELECT * FROM noticias WHERE id_noticia = 2', callback);
+NoticiasDAO.prototype.getNoticia = function(id_noticia, callback){
+	console.log(id_noticia.id_noticia);
+	this._connection.query('select * from noticias where id_noticia = ' + id_noticia.id_noticia, callback);
 }
 
 NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
-    this._connection.query('INSERT INTO noticias SET ?', noticia, callback);
+	this._connection.query('insert into noticias set ? ', noticia, callback)
+}
+
+NoticiasDAO.prototype.get5UltimasNoticias = function(callback){
+	this._connection.query('select * from noticias order by data_criacao desc limit 5', callback);
 }
 
 module.exports = function(){
-    return NoticiasDAO;
+	return NoticiasDAO;
 }
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,12 +3,13 @@ var mysql = require('mysql');
 var connMySQL = function(){
     return connection = mysql.createConnection({
         host: 'localhost',
-        user: 'app_portalnews',
-        password: 'app_portal@news',
+        user: 'root',
+        password: 'root',
         database: 'portal_noticias'
     });
 }
 
 module.exports = function(){
+    console.log('Conexão estabelecida com MySQL');
     return connMySQL;
 }
